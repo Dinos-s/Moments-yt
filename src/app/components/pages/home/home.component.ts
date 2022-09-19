20a8f9e44dcd726ac678509684c3dbaf7a41b48f
Gabriel <gabriel02ramos@gmail.com>
2022-09-19 09:58:36 -0300

    Aprimorando a home-page e resgatando dados BD

--- src/app/components/pages/home/home.component.ts
+++ src/app/components/pages/home/home.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { MomentService } from "src/app/services/moment.service";
+import { IMoment } from 'src/app/IMoment';
+import { environment } from 'src/environments/environment';
+import { faSearch } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +11,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  allMoments: IMoment[] = [];
+  moments: IMoment[] = [];
+  baseApiUrl = environment.baseApiUrl
+
+  constructor(private momentService: MomentService) { }
 
   ngOnInit(): void {
+    this.momentService.getMoments().subscribe((items) => {
+      const data = items.data;
+
+      //reorganizando a data (day-month-year)
+      data.map((item) => {
+        item.created_at = new Date(item.created_at!).toLocaleDateString('pt-Br');
+      })
+
+      this.allMoments = data;//recebendo a data modificada;
+      this.moments = data;
+    })
   }
 
 }
--- src/app/services/moment.service.ts
+++ src/app/services/moment.service.ts
@@ -3,6 +3,7 @@ import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { IMoment } from '../IMoment';
 import { environment } from 'src/environments/environment';
+import { IResponse } from '../IResponse';
 
 @Injectable({
   providedIn: 'root'
@@ -13,7 +14,11 @@ export class MomentService {
 
   constructor(private http: HttpClient) { }
 
+  getMoments(): Observable<IResponse<IMoment[]>> {
+    return this.http.get<IResponse<IMoment[]>>(this.apiUrl)
+  }//pega todos os momentos cadastros;
+
   createMoment(formData: FormData): Observable<FormData>{
     return this.http.post<FormData>(this.apiUrl, formData)
-  }
+  }//cria um momento no DB;
 }
